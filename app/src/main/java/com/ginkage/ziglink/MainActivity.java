24d8bfc436f5c2c08ab2a57fdf9e50326b74d43a
Ivan Podogov <ginkage@yandex.ru>
2020-10-19 17:34:13 +0100

    Action test works, the rest doesn't

--- app/src/main/java/com/ginkage/ziglink/MainActivity.java
+++ app/src/main/java/com/ginkage/ziglink/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ginkage.ziglink;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -18,19 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
         final Intent intent = getIntent();
         final String action = intent.getAction();
+        final String query = Intent.ACTION_VIEW.equals(action) ? intent.getData().getQuery() : null;
 
         connection = new NotificationServiceConnection(this, service -> {
-            if (ACTION_TOGGLE.equals(action)) {
-                String state = intent.getStringExtra("state");
-                if ("LIGHTS_ON".equals(state)) {
-                    connection.turnOn();
-                } else if ("LIGHTS_OFF".equals(state)) {
-                    connection.turnOff();
-                }
+            if ("on".equals(query)) {
+                connection.turnOn();
+                finish();
+            } else if ("off".equals(query)) {
+                connection.turnOff();
+                finish();
             }
         });
 
-        if (Intent.ACTION_MAIN.equals(action) || ACTION_TOGGLE.equals(action)) {
+        if (Intent.ACTION_MAIN.equals(action) || query != null) {
             setContentView(R.layout.activity_main);
         } else {
             finish();
